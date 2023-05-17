4a90423daabb3a55e9ecf330d7e6ca0e34a155c0
Dan Macumber <dan.macumber@gmail.com>
2023-05-16 18:58:58 -0600

    Update copyright and version

--- plugin/openstudio.rb
+++ plugin/openstudio.rb
@@ -32,7 +32,7 @@ require 'rbconfig'
 
 module OpenStudio
   SKETCHUPPLUGIN_NAME = "OpenStudio"
-  SKETCHUPPLUGIN_VERSION = "1.4.0"
+  SKETCHUPPLUGIN_VERSION = "1.6.0"
   SKETCHUPPLUGIN_LAUNCH_GETTING_STARTED_ON_START = false
 end
 
@@ -41,7 +41,7 @@ ext.name = OpenStudio::SKETCHUPPLUGIN_NAME
 ext.description = "Adds building energy modeling capabilities by coupling SketchUp to the OpenStudio suite of tools.  \r\n\r\nVisit openstudio.net for more information."
 ext.version = OpenStudio::SKETCHUPPLUGIN_VERSION
 ext.creator = "OpenStudio Coalition"
-ext.copyright = "2008-2022, OpenStudio Coalition and other contributors."
+ext.copyright = "2008-2023, OpenStudio Coalition and other contributors."
 
 # 'true' automatically loads the extension the first time it is registered, e.g., after install
 Sketchup.register_extension(ext, true)
