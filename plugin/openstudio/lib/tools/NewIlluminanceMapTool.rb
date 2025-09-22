85359b9e2813d54ca20bbaf0f60a2857021d3cbe
Dan Macumber <dan.macumber@gmail.com>
2025-09-21 22:14:03 -0600

    Update tool icons to svgs

--- plugin/openstudio/lib/tools/NewIlluminanceMapTool.rb
+++ plugin/openstudio/lib/tools/NewIlluminanceMapTool.rb
@@ -11,7 +11,7 @@ module OpenStudio
   class NewIlluminanceMapTool < Tool
 
     def initialize
-      @cursor = UI.create_cursor(Plugin.dir + "/lib/resources/icons/OriginToolCursor-14x20.tiff", 3, 3)
+      @cursor = UI.create_cursor(Plugin.dir + "/lib/resources/icons/OriginToolCursor.svg", 7, 7)
     end
 
     def onMouseMove(flags, x, y, view)
